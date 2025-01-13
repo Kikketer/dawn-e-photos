03f6eeb28635fa597d41dc86b0c2956718b97a66
Chris Weed <chris@cjweed.com>
2025-01-13 16:45:22 -0600

    Added imgs

--- pages/portfolio.11tydata.js
+++ pages/portfolio.11tydata.js
@@ -1,8 +1,57 @@
 export default function () {
   return {
     images: [
-      { src: '2019-09-22_0756_DSC_2152', link: 'https://dawn-e-photography.client-gallery.com/gallery/george-family' },
-      { src: '2019-09-22_0756_DSC_2152', link: 'https://dawn-e-photography.client-gallery.com/gallery/george-family' },
+      { src: 'beerman', title: 'Beerman', link: 'https://dawn-e-photography.client-gallery.com/gallery/george-family' },
+      {
+        src: 'blanchards',
+        title: 'Blanchards',
+        link: 'https://dawn-e-photography.client-gallery.com/gallery/george-family',
+      },
+      { src: 'carapia', title: 'Carapia', link: 'https://dawn-e-photography.client-gallery.com/gallery/george-family' },
+      {
+        src: 'fllregionals',
+        title: 'FLL Regionals',
+        link: 'https://dawn-e-photography.client-gallery.com/gallery/george-family',
+      },
+      {
+        src: 'fllscrimmage',
+        title: 'FLL Scrimmage',
+        link: 'https://dawn-e-photography.client-gallery.com/gallery/george-family',
+      },
+      { src: 'george', title: 'George', link: 'https://dawn-e-photography.client-gallery.com/gallery/george-family' },
+      {
+        src: 'gilmores',
+        title: 'Gilmores',
+        link: 'https://dawn-e-photography.client-gallery.com/gallery/george-family',
+      },
+      { src: 'hoffman', title: 'Hoffman', link: 'https://dawn-e-photography.client-gallery.com/gallery/george-family' },
+      { src: 'mcgowan', title: 'Mcgowan', link: 'https://dawn-e-photography.client-gallery.com/gallery/george-family' },
+      {
+        src: 'schmidts',
+        title: 'Schmidts',
+        link: 'https://dawn-e-photography.client-gallery.com/gallery/george-family',
+      },
+      { src: 'wright', title: 'Wright', link: 'https://dawn-e-photography.client-gallery.com/gallery/george-family' },
+      { src: 'coyle', title: 'Coyle', link: 'https://dawn-e-photography.client-gallery.com/gallery/george-family' },
+      {
+        src: 'elibday',
+        title: 'His Birthday',
+        link: 'https://dawn-e-photography.client-gallery.com/gallery/george-family',
+      },
+      {
+        src: 'ginnybday',
+        title: 'Her Birthday',
+        link: 'https://dawn-e-photography.client-gallery.com/gallery/george-family',
+      },
+      { src: 'goble', title: 'Goble', link: 'https://dawn-e-photography.client-gallery.com/gallery/george-family' },
+      {
+        src: 'martinez',
+        title: 'Martinez',
+        link: 'https://dawn-e-photography.client-gallery.com/gallery/george-family',
+      },
+      { src: 'rumble', title: 'Rumble', link: 'https://dawn-e-photography.client-gallery.com/gallery/george-family' },
+      { src: 'swanson', title: 'Swanson', link: 'https://dawn-e-photography.client-gallery.com/gallery/george-family' },
+      { src: 'vanci', title: 'Vanci', link: 'https://dawn-e-photography.client-gallery.com/gallery/george-family' },
     ],
   }
 }
